b65672479c077b0e1632a08116d038ac819834fe
lsnomensa <lscott@nomensa.com>
2025-06-23 18:54:44 +0100

    hooks please!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express    = require('express');
 const app        = express();
 
 app.get('/', (req, res) => {
-  res.send('hello from my server with new hook!');
+  res.send('hello from my server with new hook please!');
 });
 
 const port = process.env.PORT || 3000;
